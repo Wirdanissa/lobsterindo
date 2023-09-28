91a1b93b9849df67426164ab2020c559ccb93c58
Danialwan <dani.annur.master@gmail.com>
2023-09-28 23:03:07 +0700

    MVC (CRUD_Tabel_Barang)

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -23,6 +23,30 @@ class BarangSeeder extends Seeder
             'harga'=>'180000',
             'deskripsi'=>'Lobster air tawar dengan ukuran siap konsumsi ( > 4inc )'
         ]);
+        DB::table('barang')->insert([
+            'id_penangkaran'=>'1',
+            'nama_barang'=>'Lobster Air Tawar konsumsi',
+            'jumlah_barang'=>'242', // ekor
+            'berat'=>'11', //kg
+            'harga'=>'180000',
+            'deskripsi'=>'Lobster air tawar dengan ukuran siap konsumsi ( > 4inc )'
+        ]);
+        DB::table('barang')->insert([
+            'id_penangkaran'=>'1',
+            'nama_barang'=>'Lobster Air Tawar konsumsi',
+            'jumlah_barang'=>'242', // ekor
+            'berat'=>'11', //kg
+            'harga'=>'180000',
+            'deskripsi'=>'Lobster air tawar dengan ukuran siap konsumsi ( > 4inc )'
+        ]);
+        DB::table('barang')->insert([
+            'id_penangkaran'=>'1',
+            'nama_barang'=>'Lobster Air Tawar konsumsi',
+            'jumlah_barang'=>'242', // ekor
+            'berat'=>'11', //kg
+            'harga'=>'180000',
+            'deskripsi'=>'Lobster air tawar dengan ukuran siap konsumsi ( > 4inc )'
+        ]);
             // $table->id();
             // $table->unsignedBigInteger('id_penangkaran');
             // $table->foreign('id_penangkaran')->references('id')->on('penangkaran');
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="{{ asset('css/nav.css') }}">
+
 <nav class="navbar navbar-expand-md navbar-dark bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/beranda') }}">
@@ -7,94 +9,25 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="ms-4 collapse navbar-collapse" id="navbarSupportedContent">
+
             <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav me-auto">
-                @auth
-                @if (Auth::user()->role === 'customer')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('beranda') }}">{{ __('Beranda') }}</a>
+            <ul class="ms-4 navbar-nav me-auto">
+                        <li class="nav-item me-4">
+                            <a class="nav-link" href=" ">Beranda</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('partnership.index') }}">{{ __('Partnership') }}</a>
+                        <li class="nav-item me-4">
+                            <a class="nav-link" href=" ">Partnership</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ URL('/menubelanja') }}">{{ __('Belanja') }}</a>
+                        <li class="nav-item me-4">
+                            <a class="nav-link" href=" ">Budidaya</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('riwayat.pembelian') }}">{{ __('Riwayat Pembelian') }}</a>
+                        <li class="nav-item me-4">
+                            <a class="nav-link" href=" ">Belanja</a>
                         </li>
-                        @elseif (Auth::user()->role === 'cabang')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('beranda') }}">{{ __('Beranda') }}</a>
+                        <li class="nav-item me-4">
+                            <a class="nav-link" href=" ">Riwayat Belanja</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('partnership.index') }}">{{ __('Partnership') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('pemesanan.index') }}">{{ __('Pemesanan') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('stock.index') }}">{{ __('Update Stock') }}</a>
-                        </li>
-                        @elseif (Auth::user()->role === 'kurir')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('beranda') }}">{{ __('Beranda') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('partnership.index') }}">{{ __('Partnership') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('pengiriman.index') }}">{{ __('Pengiriman') }}</a>
-                        </li>
-                        @elseif (Auth::user()->role === 'owner')
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('beranda') }}">{{ __('Beranda') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('partnership.index') }}">{{ __('Partnership') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('riwayat-penjualan.index') }}">{{ __('Penjualan') }}</a>
-                        </li>
-                    @endif
-                @endauth
-            </ul>
-
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ms-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <!-- Menu items for guest -->
-                    @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login or Register') }}</a>
-                        </li>
-                    @endif
-
-                @else
-                    <!-- Menu items for authenticated users -->
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-                            <a class="dropdown-item" href="{{ route('profile.index') }}">
-                                {{ __('Profile') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
             </ul>
         </div>
     </div>
