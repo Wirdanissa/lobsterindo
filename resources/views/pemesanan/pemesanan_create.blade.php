d530a1804a5f9c69b817ebc51097298224475101
Danialwan <dani.annur.master@gmail.com>
2023-10-26 10:01:13 +0700

    tes

--- resources/views/pemesanan/pemesanan_create.blade.php
+++ resources/views/pemesanan/pemesanan_create.blade.php
@@ -11,37 +11,20 @@
         <h1>Id barang = {{ $data->id }}</h1>
         <div class="mb-3">
             <th>id</th>
-                <th>pembayaran</th>
-                <th>user</th>
-                <th>pemesanan</th>
-                <th>pembayaran</th>
-                <th>pengiriman</th>
-                <th>jumlah</th>
-                <th>total_pembayaran</th>
-                <th>alamat</th>
-                <th>deskripsi</th>
-                <th>>stat_pembayaran</th>
-                <th>stat_pemesanan</th>
-                <th>stat_pengiriman</th>
-                <th>no_pembayaran</th>
             <label for="id_barang" class="form-label">pembayaran</label>
             <input type="text" class="form-control" id="id_barang" name="id_barang" value="{{ Session::get('id_barang') }}">
         </div>
         <div class="mb-3">
-            <label for="id_pembayaran" class="form-label">user</label>
-            <input type="text" class="form-control" id="nama_barang" name="nama_barang" value="{{ Session::get('nama_barang') }}">
+            <label for="id_pembayaran" class="form-label">id user</label>
+            <input type="text" class="form-control" id="id_user" name="id_user" value="{{ Session::get('id_user') }}">
         </div>
         <div class="mb-3">
             <label for="jumlah_barang" class="form-label">jumlah</label>
-            <input type="number" class="form-control" id="jumlah_barang" name="jumlah_barang" value="{{ Session::get('jumlah_barang') }}">
+            <input type="number" max="{{ $data->jumlah_barang }}" class="form-control" id="jumlah_barang" name="jumlah_barang" value="{{ Session::get('jumlah_barang') }}">
         </div>
         <div class="mb-3">
-            <label for="berat" class="form-label">alamat</label>
-            <input type="number" class="form-control" id="berat" name="berat" value="{{ Session::get('berat') }}">
-        </div>
-        <div class="mb-3">
-            <label for="harga" class="form-label">harga</label>
-            <input type="number" class="form-control" id="harga" name="harga" value="{{ Session::get('harga') }}">
+            <label for="alamat" class="form-label">alamat</label>
+            <input type="text" class="form-control" id="alamat" name="alamat" value="{{ Session::get('alamat') }}">
         </div>
         <div class="mb-3">
             <label for="deskripsi" class="form-label">deskripsi</label>
